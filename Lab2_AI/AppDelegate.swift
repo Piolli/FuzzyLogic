757f115911abcd5294e82d1aac24ba8b154bf668
Alexandr Kamyshev <sanek655@mail.ru>
2019-09-23 10:17:51 +0700

    Initial Commit

---
+++ Lab2_AI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Lab2_AI
+//
+//  Created by Alexandr on 23/09/2019.
+//  Copyright © 2019 Alexandr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
